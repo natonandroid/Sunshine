79416f66dcf97db0a38dd8827e1af0f7b0175ab2
nataliac06 <nataliac@email.arizona.edu>
2018-04-11 11:17:10 -0700

    Firebase Job scheduled

--- S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
+++ S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
@@ -1,4 +1,4 @@
-/*
+package com.example.android.sunshine.sync;/*
  * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,13 +13,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// TODO (2) Make sure you've imported the jobdispatcher.JobService, not job.JobService
+//  (2) Make sure you've imported the jobdispatcher.JobService, not job.JobService
 
-// TODO (3) Add a class called SunshineFirebaseJobService that extends jobdispatcher.JobService
+import android.content.Context;
+import android.os.AsyncTask;
 
-//  TODO (4) Declare an ASyncTask field called mFetchWeatherTask
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
 
-//  TODO (5) Override onStartJob and within it, spawn off a separate ASyncTask to sync weather data
-//              TODO (6) Once the weather data is sync'd, call jobFinished with the appropriate arguments
+//  (3) Add a class called SunshineFirebaseJobService that extends jobdispatcher.JobService
+public class SunshineFirebaseJobService extends  JobService{
+    //   (4) Declare an ASyncTask field called mFetchWeatherTask
+    AsyncTask mFetchWeatherTask;
 
-//  TODO (7) Override onStopJob, cancel the ASyncTask if it's not null and return true
+    //   (5) Override onStartJob and within it, spawn off a separate ASyncTask to sync weather data
+    //     (6) Once the weather data is sync'd, call jobFinished with the appropriate arguments
+    @Override
+    public boolean onStartJob(final JobParameters job) {
+        mFetchWeatherTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                SunshineSyncTask.syncWeather(SunshineFirebaseJobService.this);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                jobFinished(job, false);
+            }
+        }.execute();
+        return false;
+    }
+
+    //   (7) Override onStopJob, cancel the ASyncTask if it's not null and return true
+    @Override
+    public boolean onStopJob(JobParameters job) {
+        if ( mFetchWeatherTask != null )
+            mFetchWeatherTask.cancel(true);
+        return true;
+    }
+
+
+}
--- S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -22,16 +22,47 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 
 import com.example.android.sunshine.data.WeatherContract;
+import com.firebase.jobdispatcher.Driver;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
+
+import java.util.concurrent.TimeUnit;
 
 public class SunshineSyncUtils {
 
-//  TODO (10) Add constant values to sync Sunshine every 3 - 4 hours
+//   (10) Add constant values to sync Sunshine every 3 - 4 hours
+    private static final int WEATHER_SYNC_INTERVAL_MINUTES = 3 * 60;
+    private static final int WEATHER_SYNC_INTERVAL_SECONDS = (int) TimeUnit.MINUTES.toSeconds(WEATHER_SYNC_INTERVAL_MINUTES);
+    private static final int WEATHER_SYNC_RETRY_INTERVAL = (int) TimeUnit.HOURS.toSeconds(1);
 
     private static boolean sInitialized;
 
-//  TODO (11) Add a sync tag to identify our sync job
+//   (11) Add a sync tag to identify our sync job
+    private static final String WEATHER_JOB_TAG="weather_sync_tag";
 
-//  TODO (12) Create a method to schedule our periodic weather sync
+//   (12) Create a method to schedule our periodic weather sync
+    synchronized public static void scheduleWeatherSyncJob(final Context context){
+        if ( sInitialized ) return;
+        Driver driver = new GooglePlayDriver(context);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
+
+        Job weatherDataSyncJob = dispatcher.newJobBuilder()
+                .setService(SunshineFirebaseJobService.class)
+                .setTag(WEATHER_JOB_TAG)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(
+                        WEATHER_SYNC_INTERVAL_SECONDS,
+                        WEATHER_SYNC_INTERVAL_SECONDS + WEATHER_SYNC_RETRY_INTERVAL))
+                .setReplaceCurrent(true)
+                .build();
+
+        dispatcher.schedule(weatherDataSyncJob);
+        sInitialized = true;
+    }
 
     /**
      * Creates periodic sync tasks and checks to see if an immediate sync is required. If an
@@ -50,7 +81,9 @@ public class SunshineSyncUtils {
 
         sInitialized = true;
 
-//      TODO (13) Call the method you created to schedule a periodic weather sync
+
+//       (13) Call the method you created to schedule a periodic weather sync
+        SunshineSyncUtils.scheduleWeatherSyncJob(context);
 
         /*
          * We need to check to see if our ContentProvider has data to display in our forecast
